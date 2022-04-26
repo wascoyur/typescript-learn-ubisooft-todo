43d8ea5c9e6eabf10a62aedb88bc952f443c2e02
yuriy.vasilev <#Mywasco1>
2022-04-26 17:46:59 +0500

    repair users->user in request

--- src/pages/userIitemPage.tsx
+++ src/pages/userIitemPage.tsx
@@ -1,15 +1,17 @@
 import axios from 'axios'
 import React, { FC, useEffect, useState } from 'react';
-import { IUser } from '../types/types'
+import { useNavigate, useParams } from 'react-router-dom';
+import { IUser } from '../types/types';
 
-
-const UserIitemPage: FC<JSX.Element> = () => {
-  const [user, setUser] = useState<IUser|null>();
+const UserItemPage: FC<JSX.Element> = () => {
+  const [user, setUser] = useState<IUser | null>();
+  const navigate = useNavigate();
+  const params = useParams();
 
   const fetchUser = async () => {
     try {
       await axios
-        .get<IUser>(`https://jsonplaceholder.typicode.com/user/${id}`)
+        .get<IUser>(`http://jsonplaceholder.typicode.com/users/${params.id}`)
         .then((res) => setUser(res.data));
     } catch (error) {
       alert(error);
@@ -22,4 +24,4 @@ const UserIitemPage: FC<JSX.Element> = () => {
   return <div>userIitemPage</div>;
 };
 
-export default UserIitemPage;
+export default UserItemPage;
